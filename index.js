3709c378086eafeb458cb7f9e12251d3daad89b3
Love-Kogasa <dachao_88@qq.com>
2026-08-13 12:09:32 +0800

    你猜awa

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ import createK2K from "./createK2k.js"
 import {toKatakana} from "wanakana"
 
 export async function load(zippath, dllpath, option) {
-  var aqtk = await loadAquesTalk(zippath, dllpath, option)
+  if(zippath && dllpath) {
+    var aqtk = await loadAquesTalk(zippath, dllpath, option)
+  } else {
+    console.warn("Zip和Dll路径均不能为空，AQTK将不会被加载")
+    var aqtk = {}
+  }
   var kanaify = new Converter(option.map)
   var k2k = option.k2k || {toKana: toKatakana}
   var self
   return (self = {
     run: (koe, spd) => aqtk.run(self.koe(koe), spd),
-    destroy: aqtk.destroy.bind(aqtk),
+    destroy: () => aqtk.destroy(),
     koe: (str) => k2k.toKana(kanaify.koe(str)).replaceAll(" ", ",")
   })
 }
